b62f01111590fb4c1983cb154ad42a82e7f0f998
Vitaly Shpinyov <vitas168@mail.ru>
2021-08-29 18:35:55 +0300

    Return no result if RemoteFeedLoader is deallocated

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -19,7 +19,9 @@ public final class RemoteFeedLoader: FeedLoader {
 	}
 
 	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
-		client.get(from: url) { result in
+		client.get(from: url) { [weak self] result in
+
+			guard self != nil else { return }
 
 			switch result {
 			case let .success((data, response)):
